32d66e9784d8171e60a22cd2f1b84e5ac1730002
design-park <audrey123012@gmail.com>
2025-04-18 16:26:46 +0200

    learned about useEffect

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,16 +1,31 @@
 import { useParams } from "react-router-dom";
-import styled from 'styled-components';
+import { useState, useEffect } from "react";
+import styled from "styled-components";
 
 function Detail(props) {
-  let {id} = useParams();
+  let { id } = useParams();
   let found = props.shoes.find((x) => x.id == id);
+  let [showAlert, setShowAlert] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowAlert(false);
+    }, 2000);
+  });
 
   return (
     <div className="container">
+      {showAlert==true ?
+        <div className="alert alert-warning">2초 이내 구매 시 할인</div>
+       : null}
       <div className="row">
         <div className="col-md-6">
           <img
-            src={"https://codingapple1.github.io/shop/shoes" + (found.id + 1) + ".jpg"}
+            src={
+              "https://codingapple1.github.io/shop/shoes" +
+              (found.id + 1) +
+              ".jpg"
+            }
             width="100%"
           />
         </div>
